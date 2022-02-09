f9ca585157093dde146b2d89c78beda98e672aa0
YoannaPo <yoannna.pol@gmail.com>
2022-02-09 22:36:11 +0000

    Add exercise 1

--- src/1_has_value_checker.js
+++ src/1_has_value_checker.js
@@ -1,3 +1,18 @@
-module.exports = function hasValue() {
-  // TODO Implement me.
+
+function hasValue(objectContainer, valueTocheck) {
+
+  if (Object.values(objectContainer).includes(valueTocheck)) {
+      return true;
+      //console.log("Jest");
+
+  }  else    {
+      return false;
+      //console.log("Nie ma");
+  }
+
+
 };
+
+module.exports = hasValue;
+hasValue({"hello": "bonjour", "my": "mon"}, "bonjour");
+hasValue({"hello": "bonjour", "my": "mon"}, "hello");   // false
