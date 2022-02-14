d2e3336932203b664131e87c0a0da6d2f11fb2ab
YoannaPo <yoannna.pol@gmail.com>
2022-02-14 13:12:12 +0000

    Add exercise 3

--- src/1_has_value_checker.js
+++ src/1_has_value_checker.js
@@ -1,3 +1,13 @@
+// 1. Has Value checker
+
+// Napisz funkcję hasValue, która jako argument przyjmuje obiekt i wartość, 
+//a następnie zwraca, czy podana wartość znajduje się w obiekcie. Wartość musi mieć postać wartości, nie klucza. 
+//Oto kilka przypadków testowych:
+
+// hasValue({"hello": "bonjour", "my": "mon"}, "bonjour") // true
+// hasValue({"hello": "bonjour", "my": "mon"}, "hello")   // false
+// hasValue({"hello": "bonjour", "my": "mon"}, "tasty")   // false
+
 
 function hasValue(objectContainer, valueTocheck) {
 
--- src/3_remove_ceonsored_words.js
+++ src/3_remove_ceonsored_words.js
@@ -1,3 +1,32 @@
-module.exports = function rmCensored() {
-  // TODO Implement me.
+// 3. Remove Censored Words
+
+// Napisz funkcję rmCensored, która jako argument przyjmuje obiekt, a usuwa wszelkie właściwości, których klucz lub wartość zawiera znak „*”. Musisz zmodyfikować obiekt w miejscu, a nie tworzyć nowego. Oto kilka przypadków testowych:
+
+// const swearWords = {"s*it": "m*rde", "sweet jesus": "doux jesus", "f***": "", "omg": "w*sh"};
+// rmCensored(swearWords)
+// console.log(swearWords) // { 'sweet jesus': 'doux jesus' }
+
+// const veggies = {"potato": "starch", "spin*ch": "green", "collard": "green", "tomato": "fr*it"}
+// rmCensored(veggies)
+// console.log(veggies) // { potato: 'starch', collard: 'green' } 
+
+
+function rmCensored(obj) {
+  for (let [key, value] of Object.entries(obj)){
+    if (key.includes("*") || (value.includes("*"))) {
+      delete obj[key];
+    }
+    
+  }
+  return obj;
 };
+
+const swearWords = {"s*it": "m*rde", "sweet jesus": "doux jesus", "f***": "", "omg": "w*sh"};
+rmCensored(swearWords)
+console.log(swearWords) // { 'sweet jesus': 'doux jesus' }
+
+const veggies = {"potato": "starch", "spin*ch": "green", "collard": "green", "tomato": "fr*it"}
+rmCensored(veggies)
+console.log(veggies) // { potato: 'starch', collard: 'green' } 
+
+module.exports = rmCensored;
