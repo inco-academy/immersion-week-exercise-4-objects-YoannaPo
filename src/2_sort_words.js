1988e81cf1bad17ca0bea9ddfe4a86de18de3ea5
YoannaPo <yoannna.pol@gmail.com>
2022-02-14 13:01:01 +0000

    Add exercise 2

--- src/2_sort_words.js
+++ src/2_sort_words.js
@@ -1,3 +1,43 @@
-module.exports = function sortWords() {
-  // TODO Implement me.
-};
+// 2. Sort Words
+
+// Napisz funkcję sortWords, która przyjmuje jako argument tablicę ciągów znakowych i zwraca obiekt. 
+// Obiekt ma dwa klucze: „odd” i „even”. Klucz „odd” powiązany jest z tablicą ciągów o długości nieparzystej. 
+// Klucz „even” powiązany jest z tablicą ciągów o długości parzystej. Oto kilka przypadków testowych:
+//sortWords([]) // {
+// odd:  [],
+// even: []
+// }
+
+//sortWords(["work", "hard", "mommy"]) // {
+// odd:  [ 'mommy' ],
+// even: [ 'work', 'hard' ]
+// }
+
+//sortWords(["oh!", "ah!", "hi", "my"]) // {
+// odd:  [ 'oh!', 'ah!' ],
+// even: [ 'hi', 'my' ] 
+// }
+
+
+
+function sortWords(arr) {
+  const Obj = {
+    "odd": [],
+    "even": []
+  }
+
+  for (let i=0; i<arr.length; i++) {
+    console.log(arr);
+    if(arr[i].length % 2 === 0 ){
+      Obj.even.push(arr[i]);
+
+    } else  {
+      console.log("Odd");
+    }
+  }
+
+  };
+
+sortWords(["oh!", "ah!", "hi", "my"]);
+
+module.exports = sortWords;
